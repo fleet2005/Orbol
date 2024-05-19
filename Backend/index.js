06e8e18b0441fe640ae6eea1c66eee7f7d834bed
fleet2005 <100420vishalsrinivasan.gbkm@iiitmanipur.ac.in>
2024-05-19 17:20:30 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,13 +3,13 @@ const dotenv = require("dotenv").config();
 const detailsRoutes = require('./detailsRoutes');
 const connectDB = require("./mongoconn");
 const bodyParser = require('body-parser');
+const cloudinary = require('cloudinary').v2; 
 
 const cors = require('cors');
 
 const app= express();
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
 
 app.use(cors());
 app.use(express.json());
@@ -29,6 +29,15 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
+
+cloudinary.config({
+    cloud_name: '121194883159639',
+    api_key: 'gLxIvMEBpDI9gQhfP44R00njH3c',
+    api_secret: 'dzb1fqdf6',
+    secure:true
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
